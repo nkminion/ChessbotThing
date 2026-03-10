10eb309122e81ba5e6728c8d18aa9b023541a676
nkminion <nishant.kalaichelvan@gmail.com>
2026-03-10 14:12:13 +0530

    Integrated sunfish bot

--- Server.py
+++ Server.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from ChessBot import GetBestMove
+import sunfish
+import sunfish_uci
+import time
+
+sunfish_uci.sunfish = sunfish
 
 App = FastAPI()
 
@@ -17,9 +22,32 @@ class MoveReq(BaseModel):
 	FenString: str
 	TimeRem: float
 	Increment: float
+	Mode: str
 
 @App.post('/engine/move')
 def CalculateMove(request: MoveReq):
-	EngineMove = GetBestMove(request.FenString,request.TimeRem,request.Increment)
-	print('Done!')
+	match request.Mode:
+		case 'ChessNet':
+			EngineMove = GetBestMove(request.FenString,request.TimeRem,request.Increment)
+		case 'sunfish':
+			FenParts = request.FenString.split()
+			CurrentPos = sunfish_uci.from_fen(*FenParts)
+			Hist = [CurrentPos]
+
+			MaxTime = (request.TimeRem/40.0) + request.Increment
+			AbsoluteMax = (request.TimeRem * 0.8) - 0.1
+			FinalTime = min(MaxTime,AbsoluteMax)
+
+			StartTime = time.time()
+			Searcher = sunfish.Searcher()
+			EngineMove = None
+			
+			for depth,gamma,score,move in Searcher.search(Hist):
+				ElapsedTime = time.time() - StartTime
+				if ElapsedTime > FinalTime:
+					break
+				EngineMove = move
+				
+			IsWhiteTurn = (sunfish_uci.get_color(CurrentPos) == sunfish_uci.WHITE)
+			EngineMove = sunfish_uci.render_move(EngineMove,IsWhiteTurn)
 	return {'BestMove':EngineMove}
